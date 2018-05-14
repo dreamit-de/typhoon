2c000372fddaea3e2016e37a0800cd21181f5275
Andre Veelken <andre.veelken@dreamit.de>
2018-05-14 17:14:07 +0200

    change bootkube module location to dreamit-de

--- bare-metal/container-linux/kubernetes/bootkube.tf
+++ bare-metal/container-linux/kubernetes/bootkube.tf
@@ -1,6 +1,6 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=911f4115088b7511f29221f64bf8e93bfa9ee567"
+  source = "git::https://github.com/dreamit-de/terraform-render-bootkube.git?ref=45d4f5aa00ce6041d2ebb3ef76b8dd19814dff55"
 
   cluster_name          = "${var.cluster_name}"
   api_servers           = ["${var.k8s_domain_name}"]
